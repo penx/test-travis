497e8b115555011a9f8b8557713e2f000a893ce7
Alasdair McLeay <builds@travis-ci.com>
2017-03-03 17:55:03 +0100

    Correct git tag

--- travis/master_after_success.sh
+++ travis/master_after_success.sh
@@ -1,6 +1,6 @@
 # After a successful build to master, we create a tag
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-GIT_TAG=$(git ls-remote --tags origin v$PACKAGE_VERSION)
+GIT_TAG=v$PACKAGE_VERSION
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name $GITHUB_API
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
